560b97240c432aa35f1639cad8c1f5db38d11042
dheerajarora1997 <dheeraj.arora1997@yahoo.com>
2022-01-11 17:16:49 +0530

    nav button border removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar(props) {
         <nav className={`navbar navbar-expand-lg navbar-${mode === 'dark' || mode === 'secondary' ? 'dark' : 'light'} bg-opacity-${modeOpacity[mode]} bg-${mode}`}>
             <div className="container">
                 <Link className="navbar-brand" to="/">{title}</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="border-0 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
