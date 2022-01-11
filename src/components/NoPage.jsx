64884cce16d5db565156a14ffbe54fb2ebc086f7
dheerajarora1997 <dheeraj.arora1997@yahoo.com>
2022-01-11 12:37:57 +0530

    Update NoPage.jsx

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function NoPage(props) {
     const {mode} = props;
     return (
         <div className="container">
-            <div className={`rounded mt-5 py-5 bg-${mode === 'dark' ? 'secondary' : 'light'}`}>
+            <div className={`text-center rounded mt-5 py-5 bg-${mode === 'dark' ? 'secondary' : 'light'} text-${mode === 'dark' ? 'light' : 'secondary'}`}>
                 <h3 className="display-1 text-center mb-0">
                     404
                 </h3>
                 <h4 className="h1 fw-light text-center mt-0">
                     Not Found
                 </h4>
+                <Link className={`btn btn-sm mt-4 px-2 btn-${mode === 'dark' ? 'light' : 'dark'}`} to="/">Back to Homepage</Link>
             </div>
 
         </div>
